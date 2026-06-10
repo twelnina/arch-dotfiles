15e465e62b7bdc12787d454f096589b0349c511f
twelnina <katsu12tem.dev@gmail.com>
2026-06-10 14:43:37 +0900

    feat(hypr): add workspace, media, and screenshot keybindings

--- hypr/modules/keybinds.lua
+++ hypr/modules/keybinds.lua
@@ -1,8 +1,42 @@
-hl.bind("SUPER + Q", hl.dsp.exec_cmd("kitty"))
-hl.bind("SUPER + Z", hl.dsp.exec_cmd("zen"))
-hl.bind("SUPER + E", hl.dsp.exec_cmd("kitty yazi"))
-hl.bind("SUPER + D", hl.dsp.exec_cmd("pkill rofi || ~/.config/rofi/launchers/type-1/launcher.sh"))
-hl.bind("SUPER + R", hl.dsp.exec_cmd("~/.local/bin/gsr-toggle"))
-hl.bind("SUPER + C", hl.dsp.window.kill())
-hl.bind("SUPER + V", hl.dsp.window.float({ action = "toggle" }))
-hl.bind("SUPER + M", hl.dsp.exit())
+-- Execute a command with mainMod + [A-Z]
+local mainMod = "SUPER"
+hl.bind(mainMod .. " + Q", hl.dsp.exec_cmd("kitty"))
+hl.bind(mainMod .. " + Z", hl.dsp.exec_cmd("zen"))
+hl.bind(mainMod .. " + E", hl.dsp.exec_cmd("kitty yazi"))
+hl.bind(mainMod .. " + D", hl.dsp.exec_cmd("pkill rofi || ~/.config/rofi/launchers/type-1/launcher.sh"))
+hl.bind(mainMod .. " + R", hl.dsp.exec_cmd("~/.local/bin/gsr-toggle"))
+hl.bind(mainMod .. " + C", hl.dsp.window.kill())
+hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
+hl.bind(mainMod .. " + M", hl.dsp.exit())
+
+-- Switch workspace with mainMod + [0-9]
+-- Move active window to a workspace with mainMod + SHIFT + [0-9]
+for i = 1, 10 do
+	local key = i % 10
+	hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
+	hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
+end
+
+-- Scroll through workspaces with mainMod + mouse wheel
+hl.bind(mainMod .. " + mouse_down", hl.dsp.focus({ workspace = "e+1" }))
+hl.bind(mainMod .. " + mouse_up", hl.dsp.focus({ workspace = "e-1" }))
+
+-- Move/resize windows with mainMod + LMB/RMB and dragging
+hl.bind("SUPER + mouse:272", hl.dsp.window.drag(), { mouse = true })
+hl.bind("SUPER + mouse:273", hl.dsp.window.resize(), { mouse = true })
+
+-- Laptop multimedia keys for volume and LCD brightness
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 1%+"), { repeating = true })
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 1%-"), { repeating = true })
+hl.bind("XF86AudioMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"), { locked = true })
+hl.bind("XF86MonBrightnessUp", hl.dsp.exec_cmd("brightnessctl set 5%+"))
+hl.bind("XF86MonBrightnessDown", hl.dsp.exec_cmd("brightnessctl set 5%-"))
+
+-- Requires playerctl
+hl.bind("XF86AudioPlay", hl.dsp.exec_cmd("playerctl play-pause"))
+hl.bind("XF86AudioPrev", hl.dsp.exec_cmd("playerctl previous"))
+hl.bind("XF86AudioNext", hl.dsp.exec_cmd("playerctl next"))
+
+-- Take screenshots
+hl.bind("Print", hl.dsp.exec_cmd("~/.local/bin/screenshot-monitor"))
+hl.bind("SHIFT + Print", hl.dsp.exec_cmd("grim -g \"$(slurp)\" ~/Pictures/Screenshots/$(date +'%Y%m%d_%H%M%S').png"))
