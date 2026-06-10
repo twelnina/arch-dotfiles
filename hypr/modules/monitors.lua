15163eeceeb3b91d997efefcd8aa0fb956e488da
twelnina <katsu12tem.dev@gmail.com>
2026-06-10 11:35:10 +0900

    feat(hypr): update monitor layout and mirroring

---
+++ hypr/modules/monitors.lua
@@ -0,0 +1,21 @@
+hl.monitor({
+	output = "eDP-1",
+	mode = "1920x1200@60",
+	position = "1920x0",
+	scale = 1,
+})
+
+hl.monitor({
+	output = "desc:Philips Consumer Electronics Company PHL 221E9 0x00003F99",
+	mode = "1920x1080@75",
+	position = "0x0",
+	scale = 1,
+})
+
+hl.monitor({
+	output = "",
+	mode = "preferred",
+	position = "auto",
+	scale = 1,
+	mirror = "eDP-1",
+})
