228cdaf3e19e07623798678f27f7077557ae0925
twelnina <katsu12tem.dev@gmail.com>
2026-06-14 19:52:06 +0900

    feat(hypr): add appearace settings, general, decoration

---
+++ hypr/modules/appearance.lua
@@ -0,0 +1,37 @@
+hl.config({
+	general = {
+		gaps_in = 5,
+		gaps_out = 8,
+		border_size = 0,
+		resize_on_border = false,
+		layout = "dwindle",
+	},
+
+	decoration = {
+		rounding = 10,
+		rounding_power = 3,
+		active_opacity = 1.0,
+		inactive_opacity = 0.95,
+
+		shadow = {
+			enabled = true,
+			range = 4,
+			render_power = 3,
+			color = 0xee1a1a1a,
+		},
+
+		blur = {
+			enabled = true,
+			size = 6,
+			passes = 2,
+			noise = 0.2,
+			contrast = 0.9,
+			brightness = 0.8,
+			vibrancy_darknes = 0.1696,
+		},
+	},
+
+	animations = {
+		enabled = true,
+	},
+})
---
+++ hypr/modules/windowrules.lua
@@ -0,0 +1,8 @@
+-- Hyprland-run windowrule
+hl.window_rule({
+	name = "move-hyprland-run",
+	match = { class = "hyprland-run" },
+
+	move = "20 monitor_h-120",
+	float = true,
+})
