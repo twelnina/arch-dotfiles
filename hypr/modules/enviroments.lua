f5676b04fd6084035a2d1c468904987573eb8cdf
twelnina <katsu12tem.dev@gmail.com>
2026-06-15 11:29:32 +0900

    feat(hypr): add input and enviroments modules

---
+++ hypr/modules/enviroments.lua
@@ -0,0 +1,4 @@
+hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("XCURSOR_SIZE", "18")
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("HYPRCURSOR_SIZE", "18")
---
+++ hypr/modules/input.lua
@@ -0,0 +1,17 @@
+hl.device({
+	name = "tpps/elan-trackpoint",
+	sensitivity = -0.4,
+})
+
+hl.device({
+	name = "epic-mouse-v1",
+	sensitivity = -0.5,
+})
+
+hl.config({
+	input = {
+		kb_layout = "us",
+		follow_mouse = 1,
+		natural_scroll = false,
+	},
+})
