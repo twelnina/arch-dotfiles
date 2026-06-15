7ace3bf4e5feef5309357c8b020f53f1b16527dc
twelnina <katsu12tem.dev@gmail.com>
2026-06-15 11:57:05 +0900

    feat(hypr): assign workspaces to specific monitors

--- hypr/modules/monitors.lua
+++ hypr/modules/monitors.lua
@@ -1,21 +1,38 @@
+local laptop_output = "eDP-1"
+local external_output = "desc:Philips Consumer Electronics Company PHL 221E9 0x00003F99"
+
 hl.monitor({
-	output = "eDP-1",
+	output = laptop_output,
 	mode = "1920x1200@60",
 	position = "1920x0",
 	scale = 1.25,
 })
 
 hl.monitor({
-	output = "desc:Philips Consumer Electronics Company PHL 221E9 0x00003F99",
+	output = external_output,
 	mode = "1920x1080@75",
 	position = "0x0",
 	scale = 1,
 })
+
+for workspace = 1, 5 do
+	hl.workspace_rule({
+		workspace = tostring(workspace),
+		monitor = laptop_output,
+	})
+end
+
+for workspace = 6, 10 do
+	hl.workspace_rule({
+		workspace = tostring(workspace),
+		monitor = external_output,
+	})
+end
 
 hl.monitor({
 	output = "",
 	mode = "preferred",
 	position = "auto",
 	scale = 1,
-	mirror = "eDP-1",
+	mirror = laptop_output,
 })
