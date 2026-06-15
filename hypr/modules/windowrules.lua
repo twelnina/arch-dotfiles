b86f318ba36bea656b96149d53b50a498410a4ab
twelnina <katsu12tem.dev@gmail.com>
2026-06-15 12:32:24 +0900

    feat(hypr): add windowrules module

--- hypr/modules/windowrules.lua
+++ hypr/modules/windowrules.lua
@@ -6,3 +6,31 @@ hl.window_rule({
 	move = "20 monitor_h-120",
 	float = true,
 })
+
+hl.window_rule({
+	name = "intellij-opacity",
+	match = { class = "jetbrains-idea" },
+	no_blur = 1,
+	opacity = 0.85,
+})
+
+hl.window_rule({
+	name = "android-studio-appearance",
+	match = { class = "jetbrains-studio" },
+	no_blur = 1,
+	opacity = 0.85,
+})
+
+hl.window_rule({
+	name = "zen-appearance",
+	match = { class = "zen" },
+	no_blur = 1,
+	opacity = 0.95,
+})
+
+hl.window_rule({
+	name = "vsc-appearance",
+	match = { class = "code" },
+	no_blur = 1,
+	opacity = 0.95,
+})
